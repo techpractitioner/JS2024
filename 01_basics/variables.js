65339cd7bf83ceb39c160542aefe4e4e538d7f25
techpractitioner <153715846+techpractitioner@users.noreply.github.com>
2024-07-22 17:22:09 +0000

    Declaration in JS

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "bharti@google.com"
+var accountPassword = "12345"
+accountKey = "Jaipur"
+var accountState
+
+//accountId = 2 //not allowed
+
+accountEmail = "Bharti@Bharti.com"
+accountPassword = "1919191919"
+accountCity = "Vrindavan"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
